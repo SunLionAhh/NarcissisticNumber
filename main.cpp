cc169efd61c756b71790791197cf4f444a25c07e
Jerry <52823432+SunLionAhh@users.noreply.github.com>
2020-03-11 16:25:59 +0800

    #include <cmath> and pow(10,3) have been used. 10的3次方

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main() {
     int count=0;
     cout<<"Here are all the Narcissistic Numbers from 100 to 999:"<<endl;
     for (int i = 100; i <= MAX; i++) {
-        int unitsDigit = i%10;//个位数
-        int tensDigit = i/10%10;//十位数
-        int hundredsDigit = i/100%10;//百位数
-        //判断是否是水仙花数
+        int unitsDigit = i%10;//unitsDigit
+        int tensDigit = i/10%10;//tensDigit
+        int hundredsDigit = i/100%10;//hundredsDigit
+        //Determine whether these numbers are Narcissistic Numbers.
         if (pow(unitsDigit,3)+pow(tensDigit,3)+pow(hundredsDigit,3)==i){
             cout<<i<<"\t"<<"是水仙花数"<<endl;
             count++;
